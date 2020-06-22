bf3683a82b1013868f41d62d78e4c4f500a4b6ea
Felipe Niehues Kniess <felipe.nkniess@gmail.com>
2020-06-22 17:54:47 -0300

    Criado os modelos

---
+++ app/Categoria.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model{
+    public    $timestamps = false;
+    protected $primaryKey = 'IDCategoria';
+    protected $table = 'categorias';
+    protected $fillable = ['IDCategoria', 'NomeCategoria', 'Descricao', 'Figura'];
+}
